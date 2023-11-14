b9cc560bbe6d0985c46199445717c411cd9b81db
DaniCMV <dmartinvaras@gmail.com>
2023-11-14 11:59:14 +0100

    Inventario con generacion y modificacón de botones (sin probar aún).

--- DemonLoopCode/Assets/Scripts/ScriptableObjects/ObjectData.cs
+++ DemonLoopCode/Assets/Scripts/ScriptableObjects/ObjectData.cs
@@ -6,7 +6,17 @@ using UnityEngine.UI;
 [CreateAssetMenu]
 public class ObjectData : ScriptableObject
 {
-    [SerializeField] private Texture icon;
+    [SerializeField] private Sprite icon;
     [TextArea]
     [SerializeField] private string description;
+
+    public Sprite Icon {  get { return icon; } }
+    public string Description { get { return description; } }
+
+    public void Click(PlayerInventory inventory) 
+    { 
+        Debug.Log(name + " was clicked");
+
+        
+    }
 }

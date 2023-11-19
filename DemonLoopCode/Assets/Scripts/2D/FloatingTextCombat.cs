98d20514fdfc58e63758f3bee75fc65a343a640c
Mario Resa <mariogr901@gmail.com>
2023-11-19 10:44:31 +0100

    Popup de datos numericos en batalla agregado

---
+++ DemonLoopCode/Assets/Scripts/2D/FloatingTextCombat.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class FloatingTextCombat : MonoBehaviour
+{
+    [SerializeField] GameObject floatingTextPF;
+    [SerializeField] float destroyTime;
+
+    public void ShowFloatingText(GameObject character, float number, Color color)
+    {
+        var go = Instantiate(floatingTextPF, character.transform.position, Quaternion.identity, character.GetComponent<Stats>().CharFloatingTextSpace.transform);
+
+        var textComponent = go.GetComponent<TextMeshProUGUI>();
+
+        textComponent.text = ((int)number).ToString();
+
+        textComponent.color = color;
+
+        Destroy(go, destroyTime);
+    }
+}

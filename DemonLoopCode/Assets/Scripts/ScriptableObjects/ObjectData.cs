aa07615b6ba7169c1989c4f589afe05373a5aa84
ivansanjose1 <61494101+ivansanjose1@users.noreply.github.com>
2023-11-24 16:40:01 +0100

    larzar item que haga daño

--- DemonLoopCode/Assets/Scripts/ScriptableObjects/ObjectData.cs
+++ DemonLoopCode/Assets/Scripts/ScriptableObjects/ObjectData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public enum ObjectTypes { Health, Mana, HealState }
+public enum ObjectTypes { Health, Mana, HealState, Throwable }
 
 [CreateAssetMenu]
 public class ObjectData : ScriptableObject
@@ -12,52 +12,59 @@ public class ObjectData : ScriptableObject
     [SerializeField] private ObjectTypes objectType;
     [SerializeField] private float baseNum;
     [SerializeField] private StateData stateAsociated;
+    [SerializeField] bool targetsToLoad = false;
 
     CombatFlow combatFlow;
     EnterBattle enterBattle;
+    PlayerInventory inventory;
+
     public Sprite Icon { get { return icon; } }
     public string Description { get { return description; } }
 
     public ObjectTypes ObjectType { get { return objectType; } }
     public float BaseNum { get { return baseNum; } }
-
+    
     // Cuando se hace click en este objeto.
     public void Click(PlayerInventory inventory)
     {
         enterBattle = GameObject.Find("System").GetComponent<EnterBattle>();
         combatFlow = GameObject.Find("System").GetComponent<CombatFlow>();
-
+        this.inventory = inventory;
         if (enterBattle.OneTime)
         {
-            UserObject(combatFlow.Character);
+            combatFlow.GenerateTargetsButtons(targetsToLoad, this);
 
-            GameObject.Find("System").GetComponent<CombatFlow>().InventoryTurn();
         }
 
-        inventory.RemoveObjectFromInventory(name.Substring(4, name.Length - 4));
 
     }
-
     public void UserObject(GameObject @character)
     {
-        Stats player = @character.GetComponent<Stats>();
+        GameObject.Find("System").GetComponent<CombatFlow>().InventoryTurn();
+        Stats target = @character.GetComponent<Stats>();
         switch (ObjectType)
         {
             case ObjectTypes.Health:
                 //Debug.Log("Pocion de Cura");
-                player.Health += BaseNum;
+                target.Health += BaseNum;
                 break;
 
             case ObjectTypes.Mana:
                 //Debug.Log("Pocion de Mana");
-                player.Mana += BaseNum;
+                target.Mana += BaseNum;
                 break;
 
             case ObjectTypes.HealState:
                 GameObject.Find("System").GetComponent<LibraryStates>().RemoveCharacterWithState(@character, ObtainStateName());
                 break;
 
+            case ObjectTypes.Throwable:
+                //En este caso le tiramos algo al enemigo
+                target.Health -= BaseNum;
+                break;
+
         }
+        inventory.RemoveObjectFromInventory(name.Substring(4, name.Length - 4));
     }//Fin de UserObject
 
     private string ObtainStateName()

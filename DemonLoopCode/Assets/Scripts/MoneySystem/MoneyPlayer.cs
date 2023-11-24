cc1cfd94d318805df89f11974dfadef25faf970b
Mario Resa <mariogr901@gmail.com>
2023-11-24 08:48:17 +0100

    Dinero actualizado para verse en el HUD

--- DemonLoopCode/Assets/Scripts/MoneySystem/MoneyPlayer.cs
+++ DemonLoopCode/Assets/Scripts/MoneySystem/MoneyPlayer.cs
@@ -1,27 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MoneyPlayer : MonoBehaviour
 {
 
-    public float money = 1.1f;
+    public float money = 0f;
 
     [SerializeField] Text textMoney;
 
-    public float Money { get { return money; } set { this.money = value; } }
+    public float Money { get { return money; } set { this.money = value; SetMoneyInText(); } }
 
     // Start is called before the first frame update
     void Start()
     {
-        textMoney.text="Money: "+Money+"€";
+        SetMoneyInText();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void SetMoneyInText()
+    {
+        textMoney.text="Money: "+ Money + " Mara";
     }
 
 

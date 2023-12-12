d2870ced93a1e1aa56a7b9ab9a12296218fea19a
DaniCMV <dmartinvaras@gmail.com>
2023-12-12 10:13:52 +0100

    mapa obligatorio despues de varias habitaciones

--- DemonLoopCode/Assets/Scripts/3D/RandomMap.cs
+++ DemonLoopCode/Assets/Scripts/3D/RandomMap.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class RandomMap : MonoBehaviour
 {
+    [Header("Mapas de selección random")]
     [SerializeField] GameObject[] maps;
 
-    [Header("El ultimo de la lista tiene que ser 0")]
+    [Header("Mapas obligatorios")]
+    [SerializeField] GameObject[] oblMaps;
+
+    [Header("El ultimo de la lista tiene que ser 100")]
     [SerializeField] int[] rarety;
+
+    static int room = 0, floor = 0;
+
+    [SerializeField] int actualroom;
 
     // Start is called before the first frame update
     void Start()
     {
-        bool finish = false;
+        actualroom = room;
 
-        for (int i = 0; i < maps.Length; i++)
+        if (room == 5)
         {
-            if (Random.value >= (float) rarety[i] / 100 && !finish)
-            {
-                Instantiate(maps[i], transform.position, Quaternion.Euler(new Vector3(0, 90, 0))); 
-                finish = true;
-            }
+            Instantiate(oblMaps[0], transform.position, Quaternion.Euler(new Vector3(0, 90, 0)));
+            room = 0;
         }
+        else
+        {
+            bool finish = false;
 
+            for (int i = 0; i < maps.Length; i++)
+            {
+                if (Random.value <= ((float)rarety[i] / 100) && !finish)
+                {
+                    Instantiate(maps[i], transform.position, Quaternion.Euler(new Vector3(0, 90, 0)));
+                    finish = true;
+                }
+            }
+
+            room++;
+        }
     }
 }

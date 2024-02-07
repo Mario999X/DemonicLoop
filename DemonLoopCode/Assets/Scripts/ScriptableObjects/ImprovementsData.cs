c0411cf071feebee362cf11c1ec63fd3211bda53
ivansanjose1 <61494101+ivansanjose1@users.noreply.github.com>
2024-02-07 23:09:34 +0100

    ya va el codigo de la tienda y tienes que comprar si o si la v1 para poder pillar la siguiente v

--- DemonLoopCode/Assets/Scripts/ScriptableObjects/ImprovementsData.cs
+++ DemonLoopCode/Assets/Scripts/ScriptableObjects/ImprovementsData.cs
@@ -28,84 +28,31 @@ public class ImprovementsData : ScriptableObject
     int powerCriticalV2 = 10;
     int powerCriticalV3 = 15;
 
-    bool riseHealthV1 = false;
-    bool riseHealthV2 = false;
-    bool riseHealthV3 = false;
 
-    bool riseManaV1 = false;
-    bool riseManaV2 = false;
-    bool riseManaV3 = false;
-
-    bool riseCriticalV1 = false;
-    bool riseCriticalV2 = false;
-    bool riseCriticalV3 = false;
-
-
+    public int idHealth = 1;
+    public int idMana = 1;
+    public int idCriticalChance = 1;
 
     private FloatingTextCombat floatingText;
 
     [TextArea]
     [SerializeField] private string description;
     [SerializeField] private float costRefined;
-    [SerializeField] private ImprovementsTypes improvementsType;
-
-    [SerializeField] bool alliesTargets = false;
-    [SerializeField] bool obtained = false;
+    [SerializeField] public ImprovementsTypes improvementsType;
+    [SerializeField] public bool isVersion;
 
     [SerializeField] GameObject buttonPrefab;
 
-    float baseDamage = 1.5f;
 
     EnterBattle enterBattle;
 
     public Sprite Icon { get { return icon; } }
-    public string Description { get { return description; } }
-    public float CostRefined { get { return costRefined; } }
+    public string Description { get { return description; } set { description = value; } }
+    public float CostRefined { get { return costRefined; } set { costRefined = value; } }
     public ImprovementsTypes ImprovementsType { get { return improvementsType; } }
 
-    public bool Obtained { get { return obtained; } }
-    public float BaseDamage { get { return baseDamage; } }
 
-    // Cuando se hace click en este objeto.
-    /*public void Click(PlayerInventory inventory)
-    {
-        floatingText = GameObject.Find("System").GetComponent<FloatingTextCombat>();
-
-        this.inventory = inventory;
-        enterBattle = GameObject.Find("System").GetComponent<EnterBattle>();
-
-        if (enterBattle.OneTime)
-        {
-            if (improvementsType == ImprovementsTypes.Revive)
-            {
-                //GameObject.Find("System").GetComponent<CombatFlow>().GeneratePlayersDefeatedButtons(this);
-            }
-            //else GameObject.Find("System").GetComponent<CombatFlow>().GenerateTargetsButtons(alliesTargets, this);
-        }
-        else
-        {
-            if (ImprovementsTypes.Throwable != ImprovementsType)
-            {
-                GameObject.Find("Inventory").transform.GetChild(1).gameObject.SetActive(true);
-
-                GameObject[] buttons = GameObject.FindGameObjectsWithTag("Buttons");
-
-                if (buttons.Length > 0)
-                {
-                    foreach (GameObject bt in buttons)
-                    {
-                        Destroy(bt);
-                    }
-                }
-
-                if (improvementsType == ImprovementsTypes.Revive)
-                {
-                    CreateButtonsPlayersDefeated(GameObject.Find("PartyButtons"));
-                }
-                else if (improvementsType != ImprovementsTypes.Throwable) CreateButtons(GameObject.Find("PartyButtons"));
-            }
-        }
-    }*/
+   
 
     void CreateButtons(GameObject spawnMoveBT)
     {
@@ -141,22 +88,8 @@ public class ImprovementsData : ScriptableObject
          }*/
     }
 
-    /*private void CreateButtonsPlayersDefeated(GameObject spawnMoveBT)
-    {
-        List<GameObject> playersDefeated = GameObject.Find("System").GetComponent<CombatFlow>().PlayersDefeated;
 
-        foreach (GameObject pl in playersDefeated)
-        {
-            GameObject bt = Instantiate(buttonPrefab, spawnMoveBT.transform.position, Quaternion.identity);
-            bt.transform.SetParent(spawnMoveBT.transform);
-            bt.name = "Ally " + pl.name;//Nombre de los botones que se van a generar
-            bt.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = pl.name.Substring(pl.name.IndexOf("_") + 1);
-            bt.GetComponent<Button>().onClick.AddListener(delegate { UserObjectS2(pl); });
-        }
-    }*/
-
-
-    public void BuyImprovement(StatsPersistenceData target)
+    public string BuyImprovement(StatsPersistenceData target)
     {
         // Podria ponerse algun id a las mejoras
         // Suponiendo que sea el prota
@@ -164,88 +97,64 @@ public class ImprovementsData : ScriptableObject
         switch (ImprovementsType)
         {
             case ImprovementsTypes.Health:
-                if (!riseHealthV1)
+                if (isVersion == true && idHealth == 1)
                 {
                     target.MaxHealth += powerHealthV1;
-                    riseHealthV1 = true;
+                    target.Health = target.MaxHealth;
+                    idHealth = 2;
                 }
-                else if (!riseHealthV2 && HasV1(target))
+                else if (isVersion == true && idHealth == 2)
                 {
                     target.MaxHealth += powerHealthV2;
-                    riseHealthV2 = true;
+                    target.Health = target.MaxHealth;
+                    idHealth = 3;
                 }
-                else if (!riseHealthV3 && HasV2(target))
+                else if (isVersion == true && idHealth == 3)
                 {
                     target.MaxHealth += powerHealthV3;
-                    riseHealthV3 = true;
+                    target.Health = target.MaxHealth;
                 }
                 break;
 
             case ImprovementsTypes.Mana:
-                if (!riseManaV1)
+                if (isVersion == true && idMana == 1)
                 {
                     target.MaxMana += powerManaV1;
-                    riseManaV1 = true;
+                    target.Mana = target.MaxMana;
+                    idMana = 2;
                 }
-                else if (!riseManaV2 && HasV1(target))
+                else if (isVersion == true && idMana == 2)
                 {
                     target.MaxMana += powerManaV2;
-                    riseManaV2 = true;
+                    target.Mana=target.MaxMana;
+                    idMana = 3;
                 }
-                else if (!riseManaV3 && HasV2(target))
+                else if (isVersion == true && idMana == 3)
                 {
                     target.MaxMana += powerManaV3;
-                    riseManaV3 = true;
+                    target.Mana = target.MaxMana;
                 }
                 break;
 
             case ImprovementsTypes.CriticalChance:
-                if (!riseCriticalV1)
+                if (isVersion==true && idCriticalChance == 1)
                 {
                     target.CriticalChance += powerCriticalV1;
-                    riseCriticalV1 = true;
+                    idCriticalChance = 2;
                 }
-                else if (!riseCriticalV2 && HasV1(target))
+                else if (isVersion == true && idCriticalChance == 2)
                 {
                     target.CriticalChance += powerCriticalV2;
-                    riseCriticalV2 = true;
+                    idCriticalChance = 3;
                 }
-                else if (!riseCriticalV3 && HasV2(target))
+                else if (isVersion == true && idCriticalChance == 3)
                 {
                     target.CriticalChance += powerCriticalV3;
-                    riseCriticalV3 = true;
                 }
                 break;
         }//Fin del switch
+
+        return this.name;
     }
 
-    private bool HasV1(StatsPersistenceData target)
-    {
-        switch (ImprovementsType)
-        {
-            case ImprovementsTypes.Health:
-                return riseHealthV1;
-            case ImprovementsTypes.Mana:
-                return riseManaV1;
-            case ImprovementsTypes.CriticalChance:
-                return riseCriticalV1;
-            default:
-                return false;
-        }
-    }
-
-    private bool HasV2(StatsPersistenceData target)
-    {
-        switch (ImprovementsType)
-        {
-            case ImprovementsTypes.Health:
-                return riseHealthV2;
-            case ImprovementsTypes.Mana:
-                return riseManaV2;
-            case ImprovementsTypes.CriticalChance:
-                return riseCriticalV2;
-            default:
-                return false;
-        }
-    }
 }

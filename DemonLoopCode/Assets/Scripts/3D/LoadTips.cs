7a2f706f735134c06e0a5c7753e453a5367d218b
ivansanjose1 <61494101+ivansanjose1@users.noreply.github.com>
2024-02-27 20:04:02 +0100

    Tips en pantallas de carga usando csv

--- DemonLoopCode/Assets/Scripts/3D/LoadTips.cs
+++ DemonLoopCode/Assets/Scripts/3D/LoadTips.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadTips : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject loadingScreen;
+    TextMeshProUGUI tipsLoad;
+    List<string> tipsList=new List<string>();
+
     void Start()
     {
+        // Primer GetChild(0) = LoadingScreen,
+        // Segundo GetChild(0) = Fondo
+        // Tercer GetChild(0) = Text
+        loadingScreen = GameObject.Find("System").transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+        tipsLoad = loadingScreen.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        Debug.Log("tipsLoad " + tipsLoad.name);
+        // Directorio donde esta el Csv
         
+        string fileName = "Tips.csv";
+        string folderName = "Csv";
+        string folderPath = Path.Combine(Application.persistentDataPath, folderName);
+        Debug.Log("folderPath " + folderPath);
+        string filePath = Path.Combine(folderPath, fileName);
+        Debug.Log("filePath " + filePath);
+
+        StreamReader fileRead = new StreamReader(filePath);
+        string separador = ",";
+        string line;
+        fileRead.ReadLine();
+
+        while ((line = fileRead.ReadLine()) != null)
+        {
+            string[] row = line.Split(separador);
+            string text = row[0];
+            Debug.Log("text "+ text);
+            tipsList.Add(text);
+        }
+
+        fileRead.Close();
+
+        if (tipsList.Count > 0)
+        {
+            string random = tipsList[UnityEngine.Random.Range(0, tipsList.Count)];
+            tipsLoad.text = random;
+        }
+        else
+        {
+            Debug.Log("loadingScreen "+ loadingScreen.name);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

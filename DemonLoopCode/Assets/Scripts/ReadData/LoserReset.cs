3490977f1dd61a50a76a0a82691f405f2ccb88ed
Loy-Angel <loy.angel.puppet@gmail.com>
2024-03-05 17:58:46 +0100

    Reset stats and states

--- DemonLoopCode/Assets/Scripts/ReadData/LoserReset.cs
+++ DemonLoopCode/Assets/Scripts/ReadData/LoserReset.cs
@@ -26,6 +26,75 @@ public class LoserReset : MonoBehaviour
         // Cuando morimos se reinicia los StatsPersistenceData
         // al que tenia cuando se inicio el juego
         // Remueve todos los aliados del jugador.
+        Data.Instance.CharactersTeamStats.ForEach(x => 
+        { 
+            x.Level = 1; 
+            x.ActualStates.Clear(); 
+            
+            if (!x.Protagonist)
+            {
+                x.CurrentXP = 0;
+                x.Health = 20;
+                x.MaxHealth = 20;
+                x.Mana = 20;
+                x.MaxMana = 20;
+                x.SP = 0;
+                x.Strenght = 20;
+                x.PhysicalDefense = 20;
+                x.MagicAtk = 20;
+                x.MagicDef = 20;
+                x.CriticalChance = 10;
+            }
+            else
+            {
+                x.CurrentXP = 0;
+                x.Health = 300;
+                x.MaxHealth = 300;
+                x.Mana = 150;
+                x.MaxMana = 150;
+                x.SP = 0;
+                x.Strenght = 25;
+                x.PhysicalDefense = 25;
+                x.MagicAtk = 25;
+                x.MagicDef = 25;
+                x.CriticalChance = 10;
+            }
+        });
+        Data.Instance.CharactersBackupStats.ForEach(x => 
+        { 
+            x.Level = 1; 
+            x.ActualStates.Clear();
+
+            if (!x.Protagonist)
+            {
+                x.CurrentXP = 0;
+                x.Health = 20;
+                x.MaxHealth = 20;
+                x.Mana = 20;
+                x.MaxMana = 20;
+                x.SP = 0;
+                x.Strenght = 20;
+                x.PhysicalDefense = 20;
+                x.MagicAtk = 20;
+                x.MagicDef = 20;
+                x.CriticalChance = 10;
+            }
+            else
+            {
+                x.CurrentXP = 0;
+                x.Health = 300;
+                x.MaxHealth = 300;
+                x.Mana = 150;
+                x.MaxMana = 150;
+                x.SP = 0;
+                x.Strenght = 25;
+                x.PhysicalDefense = 25;
+                x.MagicAtk = 25;
+                x.MagicDef = 25;
+                x.CriticalChance = 10;
+            }
+        });
+
         Data.Instance.CharactersTeamStats.RemoveAll(x => !x.Protagonist);
         Data.Instance.CharactersBackupStats.RemoveAll(x => !x.Protagonist);
 
@@ -34,9 +103,7 @@ public class LoserReset : MonoBehaviour
 
         // Vacia el inventario del jugador.
         if (playerInventory != null)
-        {
             playerInventory.ResetInventario();
-        }
 
         // Reinicia los datos de generacion de salas.
         Data.Instance.BossRoom = 4;
